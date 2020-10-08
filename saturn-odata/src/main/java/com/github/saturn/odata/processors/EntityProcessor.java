fe6d98486d6bfcbd4fb88897db0061980aecf1c6
gnahZ-eH <hershxy@sjtu.edu.cn>
2020-10-08 14:55:20 +0800

    🧩: Finish method (private)readEntity() in EntityProcessor.

--- saturn-odata/src/main/java/com/github/saturn/odata/processors/EntityProcessor.java
+++ saturn-odata/src/main/java/com/github/saturn/odata/processors/EntityProcessor.java
@@ -24,28 +24,44 @@
 
 package com.github.saturn.odata.processors;
 
+import com.github.saturn.odata.annotations.ODataEntityType;
 import com.github.saturn.odata.annotations.ODataFunction;
+import com.github.saturn.odata.exceptions.SaturnODataException;
 import com.github.saturn.odata.interfaces.CustomOperation;
 import com.github.saturn.odata.interfaces.EntityOperation;
 import com.github.saturn.odata.metadata.SaturnEdmContext;
+import com.github.saturn.odata.uri.QueryOptions;
+import com.github.saturn.odata.utils.ExceptionUtils;
 
+import org.apache.olingo.commons.api.data.ContextURL;
+import org.apache.olingo.commons.api.data.Entity;
 import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
 import org.apache.olingo.commons.api.format.ContentType;
+import org.apache.olingo.commons.api.http.HttpHeader;
+import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.ODataApplicationException;
 import org.apache.olingo.server.api.ODataLibraryException;
 import org.apache.olingo.server.api.ODataRequest;
 import org.apache.olingo.server.api.ODataResponse;
 import org.apache.olingo.server.api.processor.EntityCollectionProcessor;
+import org.apache.olingo.server.api.serializer.EntitySerializerOptions;
+import org.apache.olingo.server.api.serializer.ODataSerializer;
+import org.apache.olingo.server.api.serializer.SerializerException;
+import org.apache.olingo.server.api.serializer.SerializerResult;
 import org.apache.olingo.server.api.uri.UriInfo;
+import org.apache.olingo.server.api.uri.UriParameter;
 import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceEntitySet;
 import org.apache.olingo.server.api.uri.queryoption.ExpandOption;
 import org.apache.olingo.server.api.uri.queryoption.SelectOption;
 import org.springframework.context.ApplicationContext;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EntityProcessor extends SaturnProcessor implements org.apache.olingo.server.api.processor.EntityProcessor, EntityCollectionProcessor {
 
@@ -80,7 +96,8 @@ public class EntityProcessor extends SaturnProcessor implements org.apache.oling
 //        }
     }
 
-    private void readEntity(ODataResponse oDataResponse, UriInfo uriInfo, ContentType contentType) {
+    // todo need to be tested
+    private void readEntity(ODataResponse oDataResponse, UriInfo uriInfo, ContentType contentType) throws SaturnODataException {
 
         List<UriResource> uriResourceParts = uriInfo.getUriResourceParts();
         UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) uriResourceParts.get(0);
@@ -91,8 +108,71 @@ public class EntityProcessor extends SaturnProcessor implements org.apache.oling
         ExpandOption expandOption = uriInfo.getExpandOption();
         String selects;
 
-        // todo
+        try {
+            selects = getOData().createUriHelper().buildContextURLSelectList(edmEntityType, null, selectOption);
+        } catch (SerializerException e) {
+            throw new SaturnODataException(HttpStatusCode.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
 
+        Entity entity;
+        Object object = readByEntityOperation(uriResourceEntitySet, selectOption, expandOption);
+        ExceptionUtils.assertNotNull(object, ODataEntityType.class.getSimpleName());
+
+        try {
+            entity = fromObject2Entity(object, expandOption);
+        } catch (IllegalAccessException e) {
+            throw new SaturnODataException(HttpStatusCode.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+        ContextURL contextURL;
+
+        try {
+            contextURL = ContextURL
+                    .with()
+                    .serviceRoot(new URI(getSaturnEdmContext().getServiceRoot()))
+                    .entitySet(edmEntitySet)
+                    .selectList(selects)
+                    .suffix(ContextURL.Suffix.ENTITY)
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new SaturnODataException(HttpStatusCode.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+        EntitySerializerOptions serializerOptions = EntitySerializerOptions
+                .with()
+                .contextURL(contextURL)
+                .select(selectOption)
+                .expand(expandOption)
+                .build();
+
+        ODataSerializer oDataSerializer;
+        SerializerResult serializerResult;
+
+        try {
+            oDataSerializer = getOData().createSerializer(contentType);
+            serializerResult = oDataSerializer.entity(getServiceMetadata(), edmEntityType, entity, serializerOptions);
+        } catch (SerializerException e) {
+            throw new SaturnODataException(HttpStatusCode.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+        oDataResponse.setHeader(HttpHeader.CONTENT_TYPE, contentType.toContentTypeString());
+        oDataResponse.setContent(serializerResult.getContent());
+        oDataResponse.setStatusCode(HttpStatusCode.OK.getStatusCode());
+    }
+
+    private Object readByEntityOperation(UriResourceEntitySet uriResourceEntitySet, SelectOption selectOption, ExpandOption expandOption) throws SaturnODataException {
+
+        // can also use EntitySet
+        // todo need to test here
+        EdmEntityType edmEntityType = uriResourceEntitySet.getEntityType();
+        EntityOperation entityOperation = entityOperationMap.get(edmEntityType.getName());
+
+        ExceptionUtils.assertNotNull(entityOperation, EntityOperation.class.getSimpleName(), edmEntityType.getName());
+        List<UriParameter> parameters = uriResourceEntitySet.getKeyPredicates();
+        Map<String, UriParameter> parameterMap = parameters.stream().collect(Collectors.toMap(UriParameter::getName, p -> p));
+        QueryOptions queryOptions = new QueryOptions(expandOption, null, selectOption, null);
+
+        return entityOperation.retrieveByKey(parameterMap, queryOptions, null);
     }
 
     @Override
